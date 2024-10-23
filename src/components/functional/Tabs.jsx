5a09a4b4734d55db0e3862d9ec709418f8056476
Thuzar <thuzar.ts92@gmail.com>
2024-10-23 13:52:22 +0700

    Document Tabs

--- src/components/functional/Tabs.jsx
+++ src/components/functional/Tabs.jsx
@@ -7,10 +7,10 @@ const Tabs = () => {
   return (
     <div className="pb-10">
       <div className="w-full">
-        <div className="mb-4 flex space-x-4 p-2 bg-white shadow-box">
+        <div className="mb-4 grid grid-cols-2 gap-4 md:grid-cols-4 p-2 bg-white shadow-box">
           <button
             onClick={() => setOpenTab(1)}
-            className={`flex-1 py-2 px-4 rounded-md focus:outline-none focus:shadow-outline-blue transition-all duration-300 hover:bg-third-900 hover:text-white ${
+            className={`py-2 px-4 rounded-md focus:outline-none focus:shadow-outline-blue transition-all duration-300 hover:bg-third-900 hover:text-white ${
               openTab === 1 ? 'bg-third-900 text-white' : ''
             }`}
           >
@@ -18,7 +18,7 @@ const Tabs = () => {
           </button>
           <button
             onClick={() => setOpenTab(2)}
-            className={`flex-1 py-2 px-4 rounded-md focus:outline-none focus:shadow-outline-blue transition-all duration-300 hover:bg-third-900 hover:text-white ${
+            className={`py-2 px-4 rounded-md focus:outline-none focus:shadow-outline-blue transition-all duration-300 hover:bg-third-900 hover:text-white ${
               openTab === 2 ? 'bg-third-900 text-white' : ''
             }`}
           >
@@ -26,7 +26,7 @@ const Tabs = () => {
           </button>
           <button
             onClick={() => setOpenTab(3)}
-            className={`flex-1 py-2 px-4 rounded-md focus:outline-none focus:shadow-outline-blue transition-all duration-300 hover:bg-third-900 hover:text-white ${
+            className={`py-2 px-4 rounded-md focus:outline-none focus:shadow-outline-blue transition-all duration-300 hover:bg-third-900 hover:text-white ${
               openTab === 3 ? 'bg-third-900 text-white' : ''
             }`}
           >
@@ -34,7 +34,7 @@ const Tabs = () => {
           </button>
           <button
             onClick={() => setOpenTab(4)}
-            className={`flex-1 py-2 px-4 rounded-md focus:outline-none focus:shadow-outline-blue transition-all duration-300 hover:bg-third-900 hover:text-white ${
+            className={`py-2 px-4 rounded-md focus:outline-none focus:shadow-outline-blue transition-all duration-300 hover:bg-third-900 hover:text-white ${
               openTab === 4 ? 'bg-third-900 text-white' : ''
             }`}
           >
@@ -45,12 +45,12 @@ const Tabs = () => {
         {/* Tab Contents */}
         {openTab === 1 && (
           <div className="transition-all duration-300 bg-white p-4 px-0">
-            <div className="flex">
-              <div className="w-1/4">
+            <div className="md:flex">
+              <div className="max-w-sm md:w-1/4">
                 <img src={defaultImg.src} alt="Correction & Adhesion" className="w-full object-contain rounded" />
               </div>
-              <div className="w-3/4 pl-0 md:pl-10">
-                <h1 className="font-bold">2024 Catalog</h1>
+              <div className="md:w-3/4 pl-0 md:pl-10">
+                <h1 className="font-bold">2024 Catalog Correction & Adhesion</h1>
                 <p className="py-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit...</p>
                 <a
                   href="#"
@@ -65,13 +65,13 @@ const Tabs = () => {
 
         {openTab === 2 && (
           <div className="transition-all duration-300 bg-white p-4 px-0">
-            <div className="flex">
-              <div className="w-1/4">
-                <img src={defaultImg.src} alt="Office Supplies" className="w-full object-contain rounded" />
+            <div className="md:flex">
+              <div className="max-w-sm md:w-1/4">
+                <img src={defaultImg.src} alt="Correction & Adhesion" className="w-full object-contain rounded" />
               </div>
-              <div className="w-3/4 pl-0 md:pl-10">
-                <h1 className="font-bold">2024 Catalog for Office Supplies</h1>
-                <p className="py-5">Reiciendis minus saepe distinctio earum hic amet cupiditate...</p>
+              <div className="md:w-3/4 pl-0 md:pl-10">
+                <h1 className="font-bold">2024 Catalog Office Supplies</h1>
+                <p className="py-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit...</p>
                 <a
                   href="#"
                   className="px-4 py-3 bg-third-900 text-white hover:bg-third-600 font-bold rounded-md text-sm inline-block"
@@ -85,12 +85,12 @@ const Tabs = () => {
 
         {openTab === 3 && (
           <div className="transition-all duration-300 bg-white p-4 px-0">
-            <div className="flex">
-              <div className="w-1/4">
-                <img src={defaultImg.src} alt="Shearing & Cutting" className="w-full object-contain rounded" />
+            <div className="md:flex">
+              <div className="max-w-sm md:w-1/4">
+                <img src={defaultImg.src} alt="Correction & Adhesion" className="w-full object-contain rounded" />
               </div>
-              <div className="w-3/4 pl-0 md:pl-10">
-                <h1 className="font-bold">2024 Catalog for Shearing</h1>
+              <div className="md:w-3/4 pl-0 md:pl-10">
+                <h1 className="font-bold">2024 Catalog Shearing</h1>
                 <p className="py-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit...</p>
                 <a
                   href="#"
@@ -105,12 +105,12 @@ const Tabs = () => {
 
         {openTab === 4 && (
           <div className="transition-all duration-300 bg-white p-4 px-0">
-            <div className="flex">
-              <div className="w-1/4">
-                <img src={defaultImg.src} alt="Stapling & Punching" className="w-full object-contain rounded" />
+            <div className="md:flex">
+              <div className="max-w-sm md:w-1/4">
+                <img src={defaultImg.src} alt="Correction & Adhesion" className="w-full object-contain rounded" />
               </div>
-              <div className="w-3/4 pl-0 md:pl-10">
-                <h1 className="font-bold">2024 Catalog for Stapling</h1>
+              <div className="md:w-3/4 pl-0 md:pl-10 mt-3 md:mt-0">
+                <h1 className="font-bold">2024 Catalog Stapling</h1>
                 <p className="py-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit...</p>
                 <a
                   href="#"
