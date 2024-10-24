7be1751f35f711bb14d183f13d1ff91260cce235
yan <yanlinnthor@evolxit.com>
2024-10-24 21:46:16 +0630

    Update product details

--- src/components/functional/ProductDetails.jsx
+++ src/components/functional/ProductDetails.jsx
@@ -7,95 +7,82 @@ const searchParams = new URLSearchParams(queryString);
 const productId = searchParams.get('product') ?? '';
 
 const ProductDetails = () => {
-  const [product, setProduct] = useState('');
+  const [product, setProduct] = useState();
   useEffect(() => {
     getProduct(productId);
   }, []);
 
-  // let keys = [];
-
   const getProduct = async (productId) => {
     const { list } = await ApiService.getProduct(productId);
     console.log('list', list);
-    // keys = Object.keys(list.variants);
-    // console.log('keys', keys);
     setProduct(list);
   };
 
-  // const getVariants = (variants) => {
-  //   return Object.keys(variants);
-  // };
+  const getVariantKeys = (variants) => {
+    return Object.keys(variants);
+  };
 
-  // console.log(product.variants);
-  // const keys = Object.keys(product.variants);
-  // console.log('keys', keys);
+  const showVariantValues = (name, variant) => {
+    if (name !== '' && name !== null) {
+      return (
+        <p>
+          <strong>{name}</strong> : {variant.join(', ')}
+        </p>
+      );
+    }
+  };
 
   return (
-    <div className="">
-      <div className="flex mb-10">
-        <div className="flex-1">
-          <img src={product.coverImage ?? defaultImg.src} />
-        </div>
-        <div className="flex-1">
-          <h1 className="mb-3 font-bold text-xl">{product.name}</h1>
-          <p>6500 ks</p>
-          <p>{product.description}</p>
+    product && (
+      <div className="">
+        <div className="flex mb-10">
+          <div className="flex-1">
+            <img src={product.coverImage ?? defaultImg.src} />
+          </div>
+          <div className="flex-1">
+            <h1 className="mb-3 font-bold text-xl">{product.name}</h1>
+            <p>6500 ks</p>
+            <p>{product.description}</p>
 
-          {/* {product.variants.colors[0]} */}
-
-          {/* {Array.isArray(product.variants) &&
-            product.variants.length > 0 &&
-            product.variants.map((variant, index) => (
+            {getVariantKeys(product.variants).map((variantName, index) => (
               <div key={index}>
-                <span>{index}</span>
-                {variant.color}
+                <p>
+                  Key: {variantName} : Value: {JSON.stringify(product.variants[variantName] ?? {})}
+                </p>
+                {showVariantValues(variantName, product.variants[variantName])}
               </div>
-            ))} */}
+            ))}
 
-          {/* {(product.variants || []).map((variant, index) => (
-            <div key={index}>
-              <p>{variant}</p>
-            </div>
-          ))} */}
-
-          {/* {getVariants(product.variants)} */}
-
-          {/* {Object.keys(product.variants).map((key, index) => (
-            <div key={index}>
-              <p>Key: {key}</p>
-              <p>Value: {product.variants[key]}</p>
-            </div>
-          ))} */}
-
-          <div className="flex">
-            <div className="w-1/3">
-              <p>SKU</p>
-              <p>Category</p>
-              <p>Tag</p>
-            </div>
-            <div className="w-2/3">
-              <p>{product.sku}</p>
-              <p>{product.category}</p>
-              <p>{product.tag}</p>
+            <div className="flex">
+              <div className="w-1/3">
+                <p>SKU</p>
+                <p>Category</p>
+                <p>Tag</p>
+              </div>
+              <div className="w-2/3">
+                <p>{product.sku}</p>
+                <p>{product.category}</p>
+                <p>{product.tag}</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <iframe
-        height="315"
-        src="https://www.youtube-nocookie.com/embed/DaxL4gYwUrU"
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
-        className="w-full mb-5 rounded-sm"
-        allowFullScreen
-      ></iframe>
+        <iframe
+          height="315"
+          src="https://www.youtube-nocookie.com/embed/DaxL4gYwUrU"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
+          className="w-full mb-5 rounded-sm"
+          allowFullScreen
+        ></iframe>
 
-      <div>
-        <h1 className="font-semibold text-lg my-3 underline">Description</h1>
-        <p className="text-sm text-gray-500">{product.description}</p>
+        <div>
+          <h1 className="font-semibold text-lg my-3 underline">Description</h1>
+          <p className="text-sm text-gray-500">{product.description}</p>
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
