bed2225fc8ff8006711f4f11cd5907558901eca2
Yan <yanlinnthor@evolxit.com>
2025-01-29 16:31:47 +0700

    add i18n

---
+++ src/i18n/ui.js
@@ -0,0 +1,29 @@
+// https://docs.astro.build/en/recipes/i18n/#recipe
+
+export const languages = {
+  en: "English",
+  mm: "မြန်မာ",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    "nav.home": "Home",
+    "nav.about": "About",
+    "nav.products": "Products",
+    "nav.contact": "Contact",
+    "nav.blog": "Blog",
+    "nav.documents": "Documents",
+    "slider.viewItem": "View Item",
+  },
+  mm: {
+    "nav.home": "ပင်မစာမျက်နှာ",
+    "nav.about": "ကျွန်ုပ်တို့အကြောင်း",
+    "nav.products": "ပစ္စည်းများ",
+    "nav.contact": "ဆက်သွယ်ရန်",
+    "nav.blog": "Blog",
+    "nav.documents": "Documents",
+    "slider.viewItem": "ကြည့်မည်",
+  },
+};
