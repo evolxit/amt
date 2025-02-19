65fe9937228df0834a137bc69408f3861261f5cb
Yan <yanlinnthor@evolxit.com>
2025-02-19 09:42:35 +0630

    ui update

--- src/components/functional/SearchBox.jsx
+++ src/components/functional/SearchBox.jsx
@@ -43,7 +43,8 @@ const SearchBox = () => {
               </h3>
               <button
                 id="closeModalButton"
-                className="text-gray-500 hover:text-gray-700"
+                className="text-gray-700 hover:text-gray-900"
+                onClick={() => setShowModal(false)}
               >
                 <svg
                   className="h-4 w-4 inline-block ml-2"
